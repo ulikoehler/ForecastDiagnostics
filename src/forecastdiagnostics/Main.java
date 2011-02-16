635bb456f6ab7bdc08a9a1bec9792d8bf84301cf
Uli Köhler <ulrich.koehler@helmholtz-muenchen.de>
2011-02-17 00:16:23 +0100

    ...

--- src/forecastdiagnostics/Main.java
+++ src/forecastdiagnostics/Main.java
@@ -4,17 +4,15 @@
  */
 package forecastdiagnostics;
 
-import com.google.common.collect.ImmutableList;
+import com.google.common.base.Splitter;
 import com.google.common.collect.Lists;
 import java.io.BufferedInputStream;
 import java.io.InputStream;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.Collection;
+import java.util.Arrays;
 import java.util.List;
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamReader;
-import org.apache.commons.io.input.AutoCloseInputStream;
 
 /**
  *
@@ -30,11 +28,11 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws Exception {
-        InputStream in = new AutoCloseInputStream(new BufferedInputStream(new URL("http://www.weather.gov/forecasts/xml/sample_products/browser_interface/ndfdXMLclient.php?listCitiesLevel=1234").openStream()));
+        InputStream in = new BufferedInputStream(new URL("http://www.weather.gov/forecasts/xml/sample_products/browser_interface/ndfdXMLclient.php?listCitiesLevel=1234").openStream());
         XMLInputFactory fac = XMLInputFactory.newFactory();
         XMLStreamReader reader = fac.createXMLStreamReader(in);
-        List<String> coordinates = null;
-        List<String> names = null;
+        List<String> coordinates = Lists.newLinkedList();
+        List<String> names = Lists.newLinkedList();
         while (reader.hasNext()) {
             int ev = reader.next();
             if (ev == XMLStreamReader.START_ELEMENT) {
@@ -48,21 +46,30 @@ public class Main {
             } else if (ev == XMLStreamReader.CHARACTERS) {
                 String text = reader.getText();
                 if (state == LATLONLIST) {
-                    String[] split = text.split(",");
-                    coordinates = ImmutableList.copyOf(split);
+                    for (String coord : Splitter.on(",").omitEmptyStrings().trimResults().split(text)) {
+                        coordinates.add(coord);
+                    }
+//                    coordinates = ImmutableList.copyOf(split);
 //                    for(String s : coordinates) {
 //                    System.out.println(s);}
                 } else if (state == CITYLIST) {
-                    String[] split = text.split("\\|");
-                    names = ImmutableList.copyOf(split);
+                    for (String name : Splitter.on("|").omitEmptyStrings().trimResults().split(text)) {
+                        names.add(name);
+                    }
+//                    names = ImmutableList.copyOf(split);
 //                    for(String s : names) {
 //                    System.out.println(s);}
                 }
             }
         }
+        in.close();
+        System.out.println("Finished");
         System.out.println(coordinates.size());
         System.out.println(names.size());
         for (int i = 0; i < coordinates.size(); i++) {
+            if (names.size() <= i) {
+                break;
+            }
             System.out.println(coordinates.get(i) + "     " + names.get(i));
         }
     }
